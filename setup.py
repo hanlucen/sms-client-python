ab09cf23f055433b012d5bd3f8ea775200e6ab63
hanlucen <hanlucen@zhumengyuan.com>
2016-11-15 11:12:48 +0800

    fix

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'Readme.md')).read()
 
 requires = [
-    'requests>=2.7',
+    'django',
     'https://github.com/hanlucen/sms-provider-python/archive/master.zip'
 ]
 
